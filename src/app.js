30ff56239c63bfda31e0576083244a463166121b
andregesualdi <andrerges@gmail.com>
2023-10-14 16:58:42 -0300

    Remoção do cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import Configuration, { configEnvironments } from "./config.js";
 import routes from "./routes/index.js";
 import moment from "moment-timezone";
-import cors from "cors";
 
 moment.tz.setDefault("America/Sao_Paulo");
 moment.locale('pt');
@@ -13,9 +12,6 @@ const port = Configuration.port;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "https://broccoli-mobile.netlify.app/"
-}));
 app.use("/brcli-gateway", routes);
 app.listen(port);
 console.log('Rodando na porta ' + port);
