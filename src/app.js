96985635a06bab35f44da68b1700ab9afd7c8c4c
andregesualdi <andrerges@gmail.com>
2023-10-14 17:15:31 -0300

    Inclusão de Cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import Configuration, { configEnvironments } from "./config.js";
 import routes from "./routes/index.js";
 import moment from "moment-timezone";
+import cors from "cors";
 
 moment.tz.setDefault("America/Sao_Paulo");
 moment.locale('pt');
@@ -12,6 +13,7 @@ const port = Configuration.port;
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/brcli-gateway", routes);
 app.listen(port);
 console.log('Rodando na porta ' + port);
