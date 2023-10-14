aa1359af3f4210a8a0c12352de8aad88538208fd
andregesualdi <andrerges@gmail.com>
2023-10-14 16:52:52 -0300

    Allow Netlify origin

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import Configuration, { configEnvironments } from "./config.js";
 import routes from "./routes/index.js";
 import moment from "moment-timezone";
+import cors from "cors";
 
 moment.tz.setDefault("America/Sao_Paulo");
 moment.locale('pt');
@@ -11,6 +12,10 @@ configEnvironments();
 const port = Configuration.port;
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "https://broccoli-mobile.netlify.app/"
+}));
 app.use("/brcli-gateway", routes);
 app.listen(port);
 console.log('Rodando na porta ' + port);
